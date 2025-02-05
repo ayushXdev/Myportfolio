9e7224aece068b55443aca10b41d426f37467d64
ayushXdev <ayush.bagg88@gmail.com>
2025-02-05 12:18:02 +0530

    Add files via upload

---
+++ portflio-react/src/Components/Navbar/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './'
+import porfile_img from '../.../assests/porfile_img.svg'
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <img src="{porfile_img}" alt="" />
+    </div>
+  )
+}
+
+export default Hero
